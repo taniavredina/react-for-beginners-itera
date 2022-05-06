9cf3ea756e89e9808f0ce968bca304e2e561c9fb
taniavredina <taniavredina@gmail.com>
2022-05-06 11:00:09 +0300

    main

--- my-page/src/App.tsx
+++ my-page/src/App.tsx
@@ -1,13 +1,38 @@
 import './App.css';
 import Records from './records.json';
+import { FC } from 'react'; 
+import { PureComponent } from 'react';
 
-const App = () => (
-  <div className='App'>
-    <h1>Hello!</h1>
-    <p>My name is {Records['First name']}. I live in {Records.City}.</p>
-    <h2>My contacts:</h2>
-    <p>{Records['E-mail']}, {Records.Phone}</p>
-  </div>
+type H1Props = {Firstname: string, City: string, Age: number}
+type H2Props = {Interests: string}
+
+const AppH1: FC<H1Props> = (props) => (
+  <div>
+  <h1>Hello!</h1>
+  <p>My name is {props.Firstname}. I live in {props.City}.</p>
+  <p>I am {props.Age} years old.</p>
+</div>
 )
 
-export default App;
+class AppH2 extends PureComponent<H2Props> {
+  render() {
+    return (
+    <>
+    <h2>My Interests:</h2>
+    <p> {this.props.Interests} </p>
+    </>
+    )
+  }
+
+}
+
+function App() {
+return (
+  <>
+  <AppH1 Firstname = {Records['First name']} City = {Records['City']} Age = {Records['Age']}/>
+  <AppH2 Interests = 'I like to travel, walk, pick mushrooms.'/></>  
+)
+}
+  
+export default App
+
